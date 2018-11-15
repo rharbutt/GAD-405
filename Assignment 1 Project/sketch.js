a55d7f9dd0c087fa589efdce5b6a6bf6bf5ef178
Ronan Harbutt <ronanharbutt@Ronans-MacBook-Pro.local>
2018-11-15 11:26:12 +0000

    Assignment 1 second update

--- Assignment 1 Project/sketch.js
+++ Assignment 1 Project/sketch.js
@@ -7,55 +7,48 @@ var yspeed = 4;
 function setup() {
   createCanvas(594, 841);
   background (255,255,255);
-
 }
-
+//Calls the moveObject function that draws and moves the shapes.
 function draw() {
-  if(tracker == 1) {
-    rect(50,50,50,50)
-  }
-  else if (tracker == 2) {
-    ellipse(50,50,50,50)
-  }
-
-// 	//Circles Size set by distance From center of screen.
-// 	var d = dist(594/2, 841/2, mouseX, mouseY);
-//
-//     //Colour of Circles
-//   	fill (random(255), random(255), random(255),random(230));
-//     strokeWeight(0)
-//   	ellipse (x, y, d, d);
-//
-//   	//Bouncing Horizontally.
-//  	 x = x + xspeed;
-//
-//   	 if (x > 594 || x < 0)  {
-//      	xspeed = -xspeed;
-//   	}
-//
-//   	//bouncing veritcally.
-//   	y = y + yspeed;
-//
-//   	if (y > 841 || y < 0) {
-// 	 	  yspeed = -yspeed;
-//   	}
-//
+  moveObject();
 }
-
+//keyboard button 1/2 = tracker 1/2 when pressed
 function keyPressed() {
   if (key == "1") {
     tracker = 1;
-    clear()
   }
   if (key == "2") {
     tracker = 2;
-    clear()
   }
 }
 
-//Organic Motion DONE
-// Random DONE
+function moveObject() {
+  // Size set by distance From center of screen.
+  var d = dist(594/2, 841/2, mouseX, mouseY);
 
-//Function needs making
-//Arrays need LEARNING
-//Loops Maybe
+    //Colour of Circles set Random
+    fill (random(255), random(255), random(255),random(230));
+    strokeWeight(0)
+
+    //if tracker 1 create rectangle else create circle
+    if(tracker == 1) {
+      rect(x,y,d,d)
+    }
+    else if (tracker == 2) {
+      ellipse(x,y,d,d)
+    }
+
+    //Bouncing Horizontally.
+   x = x + xspeed;
+
+     if (x > 594 || x < 0)  {
+      xspeed = -xspeed;
+    }
+
+    //bouncing veritcally.
+    y = y + yspeed;
+
+    if (y > 841 || y < 0) {
+      yspeed = -yspeed;
+    }
+}
